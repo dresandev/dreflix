8014375a66eebdef155a98cf436100eda1d58c9b
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2023-10-17 00:07:32 -0500

    First commit 🌓

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Fira_Sans } from 'next/font/google'
+import '@styles/reset.css'
+import '@styles/globals.css'
+
+const firaSans = Fira_Sans({
+  subsets: ['latin'],
+  weight: [
+    '400',
+    '500',
+    '700',
+  ]
+})
+
+export const metadata: Metadata = {
+  title: 'Dreflix: Explora y encuentra la película  indicada',
+  description: 'Explora a través de un gran catálogo de películas y encuentra la indicada',
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang='es'>
+      <body className={firaSans.className}>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
